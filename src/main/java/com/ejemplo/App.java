9153d1dc4dfcbc4e6e40a8f0c7c1f8966a5d756c
Juan Pablo Maldonado <jpablo770@gmail.com>
2026-09-06 20:00:46 -0400

    Proyecto inicial Maven

---
+++ src/main/java/com/ejemplo/App.java
@@ -0,0 +1,12 @@
+package com.ejemplo;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Laboratorio Jenkins Maven SonarQube");
+    }
+
+    public static String saludo(String nombre) {
+        return "Hola " + nombre;
+    }
+}
